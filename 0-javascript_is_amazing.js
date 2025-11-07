ac72978ef4c3bc5f56be46332d6dc19b601b538a
Fafali <ivyfafalilove12@gmail.com>
2025-11-07 23:33:52 +0000

    Add 0-javascript_is_amazing.js

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,2 +1,5 @@
-const myVar = "JavaScript is amazing"; // Create a constant variable
-console.log(myVar); // Print the variable to the console
+// Create a constant variable
+const myVar = "JavaScript is amazing"; 
+
+// Print the variable to the console
+console.log(myVar);
