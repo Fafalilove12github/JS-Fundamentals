143e9374233ece46a3f67282d8fa2632ca066d20
Fafalilove12github <122897675+Fafalilove12github@users.noreply.github.com>
2025-11-08 23:48:57 +0000

    Update 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,1 +1,8 @@
+const args = process.argv; // Get the arguments passed to the script
+const num = parseInt(args[2]); // Convert the first argument to an integer
 
+if (isNaN(num)) { // Check if the conversion resulted in a number
+    console.log("Not a number");
+} else {
+    console.log(`My number: ${num}`); // Print the converted number
+}
