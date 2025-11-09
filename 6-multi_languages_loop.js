d4d20936ab4a08a30362c269f9517ec4c9065db9
Fafalilove12github <122897675+Fafalilove12github@users.noreply.github.com>
2025-11-09 00:12:04 +0000

    Create 6-multi_languages_loop.js

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"]; // Create an array with the strings
+
+for (let i = 0; i < languages.length; i++) { // Loop through the array
+    console.log(languages[i]); // Print each string in the array
+}
