3bc69ffffda91de4859fd776aa63fd5262062d2f
Fafalilove12github <122897675+Fafalilove12github@users.noreply.github.com>
2025-11-09 00:32:05 +0000

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -2,7 +2,9 @@ const args = process.argv; // Get the arguments passed to the script
 const num = parseInt(args[2]); // Convert the first argument to an integer
 
 if (isNaN(num) || num <= 0) { // Check if it's not a number or less than or equal to 0
-    console.log("Missing number of occurrences");
+    if (args.length < 3) {
+        console.log("Missing number of occurrences");
+    }
 } else {
     for (let i = 0; i < num; i++) { // Loop to print the message num times
         console.log("C is fun");
